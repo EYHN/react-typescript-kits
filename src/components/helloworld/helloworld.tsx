91e333dc86983b0f8033b047503acd798a6e846d
eyhn <cneyhn@gmail.com>
2017-05-10 02:24:26 -0800

    styled-jsx

--- src/components/helloworld/helloworld.tsx
+++ src/components/helloworld/helloworld.tsx
@@ -5,7 +5,12 @@ export class Helloworld extends React.Component<undefined, undefined> {
   public render() {
     return (
       <div>
-        <h1 className={style.title}>Hello World!</h1>
+        <h1 className={style.title + ' title'}>Hello World!</h1>
+        <style jsx>{`
+          .title {
+            font-size: 100px
+          }
+        `}</style>
       </div>
     );
   }
